ab6a54cc684ae16edd3a7061a218510e63b4d091
DannyBoyNYC <daniel.deverell@gmail.com>
2019-09-30 13:36:00 -0400

    initial commit

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,37 +1,53 @@
-var gear = document.querySelector('.fa-cog')
-var options = document.querySelector('.account-dropdown ul')
+const data = {
+  recipes:
+    'Recipes. Possimus labore, officia dolore! Eaque ratione saepe, alias harum laboriosam deserunt laudantium blanditiis eum explicabo placeat reiciendis labore iste sint. Consectetur expedita dignissimos, non quos distinctio, eos rerum facilis eligendi.',
 
-gear.addEventListener('click', showOptions)
+  reviews:
+    'Reviews. Asperiores laudantium, rerum ratione consequatur, culpa consectetur possimus atque ab tempore illum non dolor nesciunt. Neque, rerum. A vel non incidunt, quod doloremque dignissimos necessitatibus aliquid laboriosam architecto at cupiditate commodi expedita in, quae blanditiis.',
+
+  delivery:
+    'Delivery. Possimus labore, officia dolore! Eaque ratione saepe, alias harum laboriosam deserunt laudantium blanditiis eum explicabo placeat reiciendis labore iste sint. Consectetur expedita dignissimos, non quos distinctio, eos rerum facilis eligendi.'
+};
+
+// the gear icon
+
+var gear = document.querySelector('.fa-cog');
+var options = document.querySelector('.account-dropdown ul');
+
+gear.addEventListener('click', showOptions);
 
 function showOptions() {
-  options.classList.toggle('active')
-  event.preventDefault()
+  options.classList.toggle('active');
+  event.preventDefault();
 }
 
+// the main menu
 
-var mainNav = document.querySelectorAll('.site-nav li')
+function clickHandler() {
+  if (!event.target.matches('.site-nav a')) return;
+  tabs.forEach(tab => tab.parentElement.classList.remove('active'));
+  event.target.parentElement.classList.add('active');
+  console.log(event.target.href);
 
-var articleArea = document.querySelector('article')
-
-
-mainNav.forEach(
-  navItem => navItem.addEventListener('click', setActive)
-)
-
-function setActive() {
-  mainNav.forEach(
-    navItem => navItem.classList.remove('active')
-  )
-  event.target.parentElement.classList.add('active')
-  console.log(event.target.href.includes('review'))
-  // use includes string method to test for review
+  // uses includes string method to test for review
   // and set the HTML on the page
-  if (event.target.href.includes('review')) {
-    articleArea.innerHTML = `
-    <p>Reviews</p>
-    `
-  } else {
-    articleArea.innerHTML = ``
+  if (event.target.href.includes('recipes')) {
+    contentPara.innerHTML = data.recipes;
+  } else if (event.target.href.includes('reviews')) {
+    contentPara.innerHTML = data.reviews;
+  } else if (event.target.href.includes('delivery')) {
+    contentPara.innerHTML = data.delivery;
   }
-  event.preventDefault()
+  event.preventDefault();
 }
+
+function makeInactive() {
+  tabs.forEach(tab => tab.classList.remove('active'));
+}
+
+var contentPara = document.querySelector('article');
+var tabs = document.querySelectorAll('.site-nav a');
+
+document.addEventListener('click', clickHandler);
+
+contentPara.innerHTML = data.recipes;
